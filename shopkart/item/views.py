44fd911caf8cfeaccbe438155e4acaca566dffd0
Ayush Pant <ayushp533@gmail.com>
2024-01-26 22:04:00 +0530

    updated views.py to display search query items

--- shopkart/item/views.py
+++ shopkart/item/views.py
@@ -9,6 +9,9 @@ def items(request):
     query = request.GET.get('query', '')
     items = Item.objects.filter(is_sold=False)
     
+    if query:
+        items = items.filter(name__icontains=query)
+    
     return render(request, 'item/items.html', {
         'items':items,
         'query':query,
